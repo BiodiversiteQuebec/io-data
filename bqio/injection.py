4a3df450b4bf172402a2c15259fb6bf3cbde109c
william Cabrera <williamjnr8708@gmail.com>
2022-05-25 15:09:52 -0400

    adding script for stac json injections

--- bqio/injection.py
+++ bqio/injection.py
@@ -3,6 +3,9 @@ sys.path.append('./bqio/')
 
 from lib import utils
 
-folder_path = "......./io-data"
-utils.push_json_to_api(folder_path,"catalogs/stac13/stac", "https://io.biodiversite-quebec.ca/stac-fast-api")
+#make sure to change to the real catalog  path configgit
+folder_parent_path = "......./io-data"
+folder_name = "catalogs/stac13/stac"
+api_host = "https://io.biodiversite-quebec.ca/stac-fast-api"
+utils.push_json_to_api(folder_parent_path,folder_name, api_host)
 
