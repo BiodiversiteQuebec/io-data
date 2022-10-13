2a134b2e2b98949c64912d9a7e5cdf1be2ed1f7e
Guillaume Larocque <glaroc@gmail.com>
2022-10-13 14:31:04 -0400

    Merge datasets

--- bqio/datasets/gbif_heatmaps.py
+++ bqio/datasets/gbif_heatmaps.py
@@ -29,32 +29,45 @@ class ThisCollection(Collection):
 	def createCollection(self):
 		"""Overrides the implementation of createCollection from Parent class (Collection)"""
 		
-		spatial_extent = pystac.SpatialExtent(bboxes=[[-81.68, -4.24 -66.55, 12.9]])
-		temporal_extent = pystac.TemporalExtent(intervals=[[datetime.fromisoformat("1990-01-01"),datetime.fromisoformat("2020-12-31")]])
+		spatial_extent = pystac.SpatialExtent(bboxes=[[-180, -90, 180, 84]])
+		temporal_extent = pystac.TemporalExtent(intervals=[[datetime.fromisoformat("1900-01-01"),datetime.fromisoformat("2022-06-01")]])
 		collection_extent = pystac.Extent(spatial=spatial_extent, temporal=temporal_extent)
-		collection_id = 'colombia_forests'
-		collection_title = "Time series of the presence of forests in Colombia"
-		collection_description = 'Yearly time series of binary forest cover in Colombia'
-		collection_license = 'CC-BY-SA-4.0'
-		collection_folder = 'colombia/forests'
+		collection_id = 'gbif_heatmaps'
+		collection_title = "Occurrence density maps created from GBIF data"
+		collection_description = 'Density maps of all occurrences in GBIF, organized by taxonomic group at an approximately 1 km resolution.'
+		collection_license = 'CC-BY-NC-4.0'
+		collection_folder = 'gbif_heatmaps'
 		collection = self.createCollectionFromParams(collection_title=collection_title, collection_description=collection_description, collection_license = collection_license, spatial_extent=spatial_extent,temporal_extent=temporal_extent,collection_extent=collection_extent, collection_id=collection_id, collection_folder=collection_folder)
-        
+		
 		return collection
 
 	def createItemList(self):
-		years = ["1990_2000","2000_2005","2005_2010","2010_2012","2012_2013","2013_2014","2014_2015","2015_2016","2016_2017","2017_2018","2018_2019","2019_2020"]
-		for y in years:
-			filename='DCCB_SMBYC_CBBQ_'+y+'.tif'
-			folder="/bqio/colombia-lc/forest/IDEAM_ForestChange/"
-			name='forests_'+y
-			uri=folder+filename
+		vars = [{"short":"all",
+				"long":"All GBIF occurrences",
+				},
+				{"short":"arthropods",
+				"long":"All GBIF arhtropod occurrences(phylum='Arthropoda')",
+				},
+				{"short":"birds",
+				"long":"All GBIF bird occurrences (class='Aves')",
+				},
+				{"short":"mammals",
+				"long":"All GBIF mammal occurrences (class='Mammalia')",
+				},
+				{"short":"plants",
+				'long':"All GBIF plant occurrences (kingdom='Plantae')",
+				},
+			]
+		for v in vars:
+			filename='gbif_'+v['short']+'_density_06-2022.tif'
+			name=v['short']+'-heatmap'
+			path='https://object-arbutus.cloud.computecanada.ca/bq-io/io/gbif-heatmaps/'+filename
 			properties = {
 				'full_filename': filename,
-				'description': 'Forest cover for Colombia for year(s) ' + y,
-				'years': y,
+                'taxa': v['short'],
+				'description': v['long'],
 			}
-			syear=y.split('_')[0]
-			newItem: ThisStacItem = ThisStacItem(name, filename, datetime.fromisoformat(syear+'-01-01'), properties, uri, "raw", False)
+			newItem: ThisStacItem = ThisStacItem(name, filename, datetime.fromisoformat('2006-01-01'), properties, path, "raw", False)
 			self.getItemList().append(newItem)
 
 		return
@@ -63,7 +76,7 @@ class ThisCollection(Collection):
 thisCollection:ThisCollection = ThisCollection()
 
 # params to create links of stac items for this collection
-host:str = "https://object-arbutus.cloud.computecanada.ca" # host name of the server stac will be located
+host:str = "https://object-arbutus.cloud.computecanada.ca/bq-io/io" # host name of the server stac will be located
 #stac_api_host = "http://localhost:8082" # host where stac api is running
 stac_api_host = "https://io.biodiversite-quebec.ca/stac/" # host where stac api is running
 
